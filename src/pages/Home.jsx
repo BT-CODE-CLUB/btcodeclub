ef6ad7d37b7119ddba863cc7b4d3bf6914bbd346
3dgyGamer <jakepark2908@gmail.com>
2023-10-05 07:35:10 -0400

    last minute fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "../components/Header";
-import Block from "../components/Block";
 import PersonCard from "../components/PersonCard";
 import Card from "../components/Card"
 // import folder from "../components/folder"
